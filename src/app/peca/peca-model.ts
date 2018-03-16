fbab5e1d0a2fe0422e790873dbe7eab25ed9c8a3
Jonathan Souza <devjonathansouzasi@gmail.com>
2018-03-16 14:17:48 -0300

    Correção do erro { TypeError: Cannot read property 'descricao' of undefined }

--- src/app/peca/peca-model.ts
+++ src/app/peca/peca-model.ts
@@ -1,5 +1,12 @@
-export interface Peca{
-    id: number;
-    descricao: string;
-    quantidade:number;
+export class Peca{
+    
+    public id: number
+    public descricao: string
+    public quantidade:number
+
+    constructor(id: number, desc: string, qtd: number){
+        this.id = id
+        this.descricao = desc
+        this.quantidade = this.quantidade    
+    }
 }
